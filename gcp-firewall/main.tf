863c0af4cc250595418409560e2b07f5038332c4
sudhir-maurya <46971361+sudhir-maurya@users.noreply.github.com>
2024-05-09 20:55:10 +0530

    Create main.tf

---
+++ gcp-firewall/main.tf
@@ -0,0 +1,12 @@
+resource "google_compute_firewall" "rules" {
+  project     = var.project_id # Replace this with your project ID in quotes
+  name        = "my-firewall-rule"
+  network     = "default"
+  description = "Creates firewall rule targeting tagged instances"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080", "1000-2000"]
+  }
+  target_tags = ["web"]
+}
